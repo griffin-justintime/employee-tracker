3c1f4c0a21e67fdd1501dddbacad170e9eba1dca
Justin Griffin <griffin.justintime@gmail.com>
2021-04-19 14:00:11 -0400

    cleaning and trying to connect

--- Main/db/index.js
+++ Main/db/index.js
@@ -1,52 +1,57 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
 
 const connection = mysql.createConnection({
-    host: 'localhost', 
+  host: "localhost",
 
-    port: 3306, 
-    user: 'root', 
-    password: '',
-    database: 'employees'
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employees",
 });
 
-const start = () => {
-    inquirer.prompt({
-        name: 'readInfo', 
-        type: 'message',
-        message: 'Type READ to see what the employees database looks like, type WRITE to add data to the database.'
-    }).then((answer) => {
-        console.log(answer);
-        console.log(answer.readInfo);
-      
-        if(answer.readInfo === "READ") {
-            connection.query('SELECT * FROM ______', (err, results) => {
-                if(err) throw err;
-                console.log(results);
-            })
-            connection.end();
-        } else if(answer.readInfo === "WRITE") {
-            connection.query('INSERT INTO products SET ?', 
-            {
-                // product_name: "",
-                // product_price: "",
-            }, (err) => {
-                if(err) throw err;
-               
-                start();
-            })
-        }
-        else {
-            console.log('PLEASE TYPE IN READ!');
-            start();
-        }
-    })
-}
+// const start = () => {
+//   inquirer
+//     .prompt({
+//       name: "readInfo",
+//       type: "message",
+//       message:
+//         "Type READ to see what the employees database looks like, type WRITE to add data to the database.",
+//     })
+//     .then((answer) => {
+//       console.log(answer);
+//       console.log(answer.readInfo);
+
+//       if (answer.readInfo === "READ") {
+//         connection.query("SELECT * FROM employees", (err, results) => {
+//           if (err) throw err;
+//           console.log(results);
+//         });
+//         connection.end();
+//       } else if (answer.readInfo === "WRITE") {
+//         connection.query(
+//           "INSERT INTO products SET ?",
+//           {
+//             // product_name: "",
+//             // product_price: "",
+//           },
+//           (err) => {
+//             if (err) throw err;
+
+//             start();
+//           }
+//         );
+//       } else {
+//         console.log("PLEASE TYPE IN READ!");
+//         start();
+//       }
+//     });
+// };
 
 connection.connect((err) => {
-    
-    if(err) throw err;
+  if (err) throw err;
 
-    console.log('Did I connect to the database?', connection.threadId);
-    start();
-})
+  console.log("Did I connect to the database?", connection.threadId);
+  start();
+});
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# employee-tracker
+# Employee-tracker
+
+## Summary
+    CLI application for managing a company's employees using node, inquirer and MySQL.
+
+    
