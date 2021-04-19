2c4575bc32cf12da58f337d29c1f7a79d066deba
Justin Griffin <griffin.justintime@gmail.com>
2021-04-19 10:51:56 -0400

    more rough table outlining

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -13,7 +13,15 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT
+    department_id INT,
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
 )
 
 SELECT * FROM ;
