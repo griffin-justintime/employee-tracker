3d76216180f9aea70eacafe60c1b6134fbc81cd8
Justin Griffin <griffin.justintime@gmail.com>
2021-04-19 11:49:01 -0400

    adding seed values

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -11,9 +11,9 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
+    department_id INT NOT NULL,
 )
 
 CREATE TABLE employee (
---
+++ Main/db/seed.sql
@@ -0,0 +1,12 @@
+use employee;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
